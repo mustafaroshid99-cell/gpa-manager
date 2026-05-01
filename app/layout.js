7cb01e48510bacf09f6ac62b64660811d09f0a75
mustafaroshid99-cell <mustafaroshid99@gmail.com>
2026-05-01 09:36:55 +0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "GPA Manager",
+  description: "Teacher GPA App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
